1a159f848d9e3c014ccd41035247f97c512f1f04
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-09-20 21:42:47 +0900

    add each with index

--- each.rb
+++ each.rb
@@ -2,12 +2,20 @@
 # 配列
 names = ["kamizato", "saitou", "tanaka"]
 
-puts "通常のeach"
+# puts "通常のeach"
+#
+# names.each do |name|
+#   puts "#{name}さん"
+# end
+#
+# puts "省略形"
+#
+# names.each { |name| puts "#{name}さん" }
 
-names.each do |name|
-  puts "#{name}さん"
+# 配列に要素を追加
+names << "yamada"
+
+# インデックス番号を一緒に表示
+names.each.with_index(1) do |name, i|
+  puts "会員No.#{i} #{name}さん"
 end
-
-puts "省略形"
-
-names.each { |name| puts "#{name}さん" }
